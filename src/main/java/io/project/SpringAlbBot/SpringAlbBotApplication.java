e39bd9d8dd99dd69ece1323aac6cbd5adb024842
GromovKG_work <kirill.gromow@yandex.ru>
2022-09-28 15:13:54 +0300

    Initial commit

---
+++ src/main/java/io/project/SpringAlbBot/SpringAlbBotApplication.java
@@ -0,0 +1,13 @@
+package io.project.SpringAlbBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlbBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlbBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/SpringAlbBot/SpringAlbBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.SpringAlbBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlbBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
